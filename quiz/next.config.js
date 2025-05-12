aea38e83bde1b912e84bc15e08dd10c0f82625a0
qbnyp <moneycloudy@naver.com>
2025-05-13 03:52:34 +0900

    :sparkles: 멘토단 문제 받기전 커밋

--- quiz/next.config.js
+++ quiz/next.config.js
@@ -3,6 +3,10 @@
  */
 const nextConfig = {
     output: 'export',
+    
+    // 여기서 basePath를 설정하면, 빌드 후에 /Quiz_ulsan/quiz/ 경로로 배포됩니다.
+    // 만약 로컬에서 실행할때 밑에꺼 활성화하면 코드가 안돌아감
+    // 배포할때만 밑에 코드 실행시킬것(basePath, assetPrefix)
     basePath: '/Quiz_ulsan/quiz',  // <- 경로 맞게
     assetPrefix: '/Quiz_ulsan/quiz',
 
